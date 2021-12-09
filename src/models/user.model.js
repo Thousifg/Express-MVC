ce5ebccfd5cff822169d61be0f4b7c67730314db
Thousifg <thousifg1999@gmail.com>
2021-12-10 00:04:32 +0530

    intial commit

---
+++ src/models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    first_name: { type: "String", required: "true" },
+    last_name: { type: "String", required: "false" },
+    gender: { type: "String", required: "true", default: "male" },
+    dob: { type: "String", required: "false" },
+    age: { type: "Number", required: "true" },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("user", userSchema);
